e151869aeffb6c16410250353931b5f25442ff30
Ricardo Galán Trejo <ricardogalantrejo@iMac-de-Ricardo.local>
2019-11-15 12:36:50 -0600

    Operaciones

---
+++ JS/clase3.js
@@ -0,0 +1,19 @@
+var edad = 36
+
+// edad = edad + 1
+
+edad += 1
+
+var peso = 62
+
+// peso = peso - 2
+
+peso -= 2
+
+var sandwich = 1
+
+// peso = peso + sandwich - jugarAlFutbol
+
+var jugarAlFutbol = 3
+
+peso = peso - jugarAlFutbol
