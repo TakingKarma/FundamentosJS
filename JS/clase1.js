5289f751f06db7294525c3c2d3629ae0b62cfd6e
Ricardo Galán Trejo <ricardogalantrejo@iMac-de-Ricardo.local>
2019-11-14 18:28:26 -0600

    se crea clase 2, srting,  substr y funciones sencillas

--- JS/clase1.js
+++ JS/clase1.js
@@ -6,7 +6,7 @@
 // console.log('Hello ' + nombre)
 // var nombre = 'JesK'
 // var apellido = 'Rodriguez'
-var nombre = 'JesK', apellido = 'Rodríguez', edad = '36'
+// var nombre = 'JesK', apellido = 'Rodríguez', edad = '36'
 
 console.log('Hello '+ ' ' +'soy' + ' ' +nombre + ' ' + apellido)
 console.log('Tengo ' + ' ' + edad + ' ' + 'años' +  ' ' + 'siendo chingona')
