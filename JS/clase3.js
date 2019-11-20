a4553ade18d7f345476689809b7b653fdb21d961
Ricardo Galán Trejo <ricardogalantrejo@iMac-de-Ricardo.local>
2019-11-20 12:03:51 -0600

    Divisiones

--- JS/clase3.js
+++ JS/clase3.js
@@ -18,6 +18,20 @@ var jugarAlFutbol = 3
 
 peso -= jugarAlFutbol
 
-var precioDeVino = 233.33
+var precioDeVino = 200.3
 
-var total = precioDeVino * 3
+var total = Math.round(precioDeVino * 100 * 3) / 100
+
+// ten en cuenta siempre 2 decimales depues de la coma
+
+var totalStr = total.toFixed(2)
+
+var total2 = parseFloat(totalStr)
+
+//División
+
+var pizza = 8
+
+var personas = 2
+
+var  entre = pizza / personas
